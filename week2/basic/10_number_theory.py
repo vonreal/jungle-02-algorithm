1f85f007930beea4cbce4c2bed0ca3fc690b5f8b
jiun <vonreal20@gmail.com>
2026-03-10 21:06:11 +0900

    feat(base/#16): 확장 유클리드 호제법 추가

--- week2/basic/10_number_theory.py
+++ week2/basic/10_number_theory.py
@@ -89,8 +89,13 @@ def extended_gcd(a, b):
     # 역추적하며 x, y 계산
     if b == 0:
         return (a, 1, 0)
-    r = a % b
-    return (a, 1, 0)
+    
+    g, x1, y1 = extended_gcd(b, a % b)
+
+    x = y1
+    y = x1 - (a // b) * y1
+
+    return (g, x, y)
 
 def is_prime(n):
     """
